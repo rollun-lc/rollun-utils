c240060fc1ecb8076191047531c20f844dc176c5
Yurii Orlyk <orlyk.rollun@gmail.com>
2020-03-27 14:23:30 +0200

    added callAttemptsTrait

---
+++ src/Utils/src/CallAttemptsTrait.php
@@ -0,0 +1,82 @@
+<?php
+
+
+namespace rollun\utils;
+
+
+/**
+ * Trait CallAttemptsTrait
+ * @package rollun\utils
+ *
+ * @todo translete comments
+ */
+trait CallAttemptsTrait
+{
+    /**
+     * Tries to call the callback function a certain number of times with a certain delay.
+     *
+     * @param int $attempts
+     * @param int $interval
+     * @param array $itemData
+     * @param bool $createIfAbsent
+     *
+     * @return array
+     *
+     * @throws \Throwable
+     * @todo move to utils
+     *
+     */
+    public static function callAttemptsCallable($attempts, $interval, callable $callback, ...$data)
+    {
+        $counter = 0;
+
+        while ($counter <= $attempts) {
+            $counter++;
+
+            try {
+                return $callback(...$data);
+            } catch (\Throwable $exception) {
+                usleep($interval + random_int(100000, 200000));
+            }
+        }
+
+        throw $exception;
+    }
+
+    /**
+     * Tries to call an object method four times with a delay of half a second.
+     *
+     * @param $method
+     * @param mixed ...$data
+     *
+     * @return array
+     *
+     * @throws \Throwable
+     */
+    public function callAttemptsMethod($method, ...$data) {
+        if (method_exists($this, $method)) {
+            return self::callAttemptsCallable(4, 500000, [$this, $method], ...$data);
+        }
+
+        throw new \Exception('method ' . $method . ' not found in ' . get_called_class() . ' class');
+    }
+
+    /**
+     * Tries to call the callback function four times with a delay of half a second.
+     * Does the same as the callAttemptsCallable method, but is called without additional parameters.
+     *
+     * @param $callable
+     * @param mixed ...$data
+     *
+     * @return array
+     *
+     * @throws \Throwable
+     */
+    public function callAttempts($callable, ...$data) {
+        if (is_callable($callable)) {
+            return self::callAttemptsCallable(4, 500000, $callable, ...$data);
+        }
+
+        throw new \Exception('First param must be callable');
+    }
+}
