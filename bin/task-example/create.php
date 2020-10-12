a38a1f53cff390f132a5f48f5bf0e741949e16c4
Yui-Ezic <mishan221199@gmail.com>
2020-10-12 20:11:14 +0300

    Fixed incorrect path to vendor/autoload

--- bin/task-example/create.php
+++ bin/task-example/create.php
@@ -2,6 +2,9 @@
 error_reporting(E_ALL ^ E_USER_DEPRECATED ^ E_DEPRECATED);
 
 chdir(dirname(dirname(__DIR__)));
-require 'vendor/autoload.php';
+
+if (!file_exists('vendor/autoload.php')) {
+    chdir(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
+}
 
 (new \rollun\Callables\TaskExample\FileSummary())->runTaskProcess((int)$argv[1]);
