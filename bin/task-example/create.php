b3bc9b00a70769fe44fbaabfe0e4336ef3496030
Yui-Ezic <mishan221199@gmail.com>
2020-10-13 11:58:51 +0300

    Fixed autoload file not included

--- bin/task-example/create.php
+++ bin/task-example/create.php
@@ -7,4 +7,6 @@ if (!file_exists('vendor/autoload.php')) {
     chdir(dirname(dirname(dirname(dirname(dirname(__DIR__))))));
 }
 
+require 'vendor/autoload.php';
+
 (new \rollun\Callables\TaskExample\FileSummary())->runTaskProcess((int)$argv[1]);
