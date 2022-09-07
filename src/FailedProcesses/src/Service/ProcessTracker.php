4b8c39dfdfb99e9e72b7162441e919509517d0ac
Maria <m.zelin.rollun@gmail.com>
2022-09-07 13:59:52 +0300

    fix(13542): write FailedProcesses metrics only for today

--- src/FailedProcesses/src/Service/ProcessTracker.php
+++ src/FailedProcesses/src/Service/ProcessTracker.php
@@ -26,9 +26,7 @@ class ProcessTracker implements MetricsProviderInterface
             return;
         }
 
-        $dirPath = static::getProcessTrackingDir();
-
-        $dirPath .= (new \DateTime())->format('Y-m-d') . '/';
+        $dirPath = static::getProcessTrackingDir() . static::getTodayDir();
 
         if (!file_exists($dirPath)) {
             $isDirCreated = mkdir($dirPath, 0777, true);
@@ -114,7 +112,7 @@ class ProcessTracker implements MetricsProviderInterface
                 Gauge::fromValue(static::getFailedProcessesCount(60))->withLabels(
                     Label::fromNameAndValue('older_than', '1_hour')
                 ),
-                Gauge::fromValue(static::getFailedProcessesCount(60 * 12))->withLabels(
+                Gauge::fromValue(static::getFailedProcessesCount(60 * 12, false))->withLabels(
                     Label::fromNameAndValue('older_than', '12_hour')
                 ),
                 Gauge::fromValue(static::getFailedProcessesCount(0))->withLabels(
@@ -189,9 +187,13 @@ class ProcessTracker implements MetricsProviderInterface
         ];
     }
 
-    private static function getFailedProcessesCount(int $passedMinutes): int
+    private static function getFailedProcessesCount(int $passedMinutes, bool $onlyToday = true): int
     {
         $dirPath = static::getProcessTrackingDir();
+
+        if ($onlyToday) {
+            $dirPath .= static::getTodayDir();
+        }
 
         $filesCount = exec("find $dirPath -type f -mmin +$passedMinutes | wc -l");
 
@@ -231,5 +233,10 @@ class ProcessTracker implements MetricsProviderInterface
     private static function getProcessTrackingDir(): string
     {
         return self::PROCESS_TRACKING_DIR;
+    }
+
+    private static function getTodayDir(): string
+    {
+        return (new \DateTime())->format('Y-m-d') . '/';
     }
 }
